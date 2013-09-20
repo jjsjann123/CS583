b3564785fcac30678923b2193330e7e30df84d47
Xiaorui Zhao <xzhao28@uic.edu>
2013-09-20 00:45:40 -0500

    Param parser (checkParams()) finished, to be tested

--- MSPS_PROJ1.py
+++ MSPS_PROJ1.py
@@ -1,14 +1,23 @@
 import re
 
-datafile = "C:\\RAYMON\\data.txt"
+datafile = "F:\\Course\\CS583\\PROJ1\\testbuild\\data.txt"
+paramfile = "F:\\Course\\CS583\\PROJ1\\testbuild\\para.txt"
 db = []
+mis = []
+sdc = []
 
 def main():
     checkSequence(datafile,db)
-    for seq in db:
-        print seq
-    print len(db)
-
+    #print db
+    #for seq in db:
+    #    print seq
+    #print len(db)
+    checkParams(paramfile,mis,sdc)
+    print mis
+    print sdc[0]
+    
+#error check TBD
+#...
 def checkSequence(filename, db):
     metadata = file(filename)
     lines = metadata.readlines()
@@ -24,6 +33,32 @@ def checkSequence(filename, db):
             itemset.append(mmm) #store the numbers
         db.append(itemset)
 
+#error check TBD:
+#1. if #sdc>1
+#2. if elements & mis's sizes cannot match
+#3. invalid number for mis & sdc
+#...
+def checkParams(filename, mis, sdc):
+    SDC_flag = False
+    metadata = file(filename)
+    lines = metadata.readlines()
+    for sline in lines:
+        
+        sline = sline.replace('\n','')
+        sline = sline.replace(' ', '')
+        #print sline
+        m = re.findall(r'\d*.\d*$',sline)
+        #if m.isdigit():
+        #    print m
+        if re.findall(r'^MIS',sline):
+            mm = map(float,m)
+            mis.append(mm[0])
+        elif SDC_flag==False and re.findall(r'^SDC',sline):
+            SDC_flag=True
+            #print m
+            mm =  map(float,m)
+            sdc.append( mm[0] )
+            
 if __name__ == "__main__":
     main()
 
